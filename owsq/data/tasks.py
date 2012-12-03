a24dc6063106c7731ae2d22bcb08f97e09f61cf3
Mark Stacy <markstacy@ou.edu>
2012-12-03 14:50:39 -0600

    Initial default Ok water survey queue

---
+++ owsq/data/tasks.py
@@ -0,0 +1,22 @@
+from celery.task import task
+from celery.task.sets import subtask
+from celery import chord
+from pymongo import Connection
+from datetime import datetime,timedelta
+from cybercom.data.catalog import datacommons #catalog
+
+mongoHost = 'localhost'
+
+
+@task()
+def add(x, y):
+    return x + y
+
+@task()
+def tsum(numbers):
+    return sum(numbers)
+@task()
+def data_workflow(num, **kwargs):
+    calback = tsum.subtask()
+    header = [add.subtask((i, i)) for i in xrange(int(num))]
+    return chord(header)(callback).get()
