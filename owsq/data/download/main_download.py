6daafc5b945e41dfdad1c5f48567a33e9a9dfba9
Mark Stacy <markstacy@ou.edu>
2013-04-09 15:16:44 -0500

    Added zip files functionality

--- owsq/data/download/main_download.py
+++ owsq/data/download/main_download.py
@@ -1,5 +1,7 @@
-import ast,os,json,imp
+import ast,os,json,imp,inspect
+import filezip
 import ConfigParser,logging
+from celery.utils.log import get_task_logger
 from subprocess import call
 #import json,urllib2,StringIO,csv,ConfigParser,os,commands
 from celery.task import task
@@ -17,9 +19,8 @@ password = config.get('user','password')
 
 mongoHost = 'localhost'
 site_database='ows'
-basedir =  '/data/static/ows_tasks/'
 @task()
-def data_download(data=None):
+def data_download(data=None,basedir='/data/static/'):
     '''
         Download multiple data sets from multiple data sources. 
             Simple cart data: Example
@@ -27,42 +28,29 @@ def data_download(data=None):
                           "parameter":"Discharge, cubic feet per second",
                            "query":"{'source':'USGS',  'webservice_type':'uv','sites':'07241800','parameterCd':'00060','startDT':'2007-10-01','endDT':'2013-04-04'}"}
                 }
+        Currently performing in a serial fashion. Need to update and perform with celery groups in which multiple parallel subtask are generated.
     '''
     #data= '''{"SCI-1":{"quantity":1,"id":"SCI-1","name":"North Canadian River at Shawnee, OK (07241800)","parameter":"Discharge, cubic feet per second","query":"{'source':'USGS','webservice_type':'uv','sites':'07241800','parameterCd':'00060','startDT':'2007-10-01','endDT':'2013-04-04'}"}}'''
     if not data:
         raise 'No Data'
     data = json.loads(data)
-    mod_dir=os.getcwd()
-    newDir = os.path.join(basedir,str(data_download.request.id))
+    module_dir=os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    newDir = os.path.join(basedir,'ows_tasks/',str(data_download.request.id))
     call(["mkdir",newDir])
-    logging.basicConfig(filename=os.path.join(newDir,'task_log.txt'),level=logging.DEBUG)
-    #call(["mkdir",newDir])
     os.chdir(newDir)
-    #dcommons = datacommons.toolkit(username,password)
-    #records= dcommons.get_data('ows',{'spec':{'data_provider':data_provider},'fields':['sources']})
-
+    logger = get_task_logger(logfile=os.path.join(newDir,'task_log.txt'))
+    #logging.basicConfig(filename=os.path.join(newDir,'task_log.txt'),level=logging.DEBUG)
+    os.chdir(newDir)
+    urls=[]
     for itm,value in data.items():
         item = ast.literal_eval(value['query'])
         try:
             query = ast.literal_eval(value['query'])
-            logging.info(value['name'] + ' -ParamCode:' + query['parameterCd'] + ' - STARTED')
-            #print os.path.join(mod_dir,'USGS'+'.py')
-            data_import=imp.load_source(item['source'],os.path.join(mod_dir,'USGS'+'.py')) # __import__(os.path.join(pwd,'USGS'))#item['source'])
-            data_import.save(value['name'],newDir,query)
-            logging.info(value['name'] + ' -ParamCode:' + query['parameterCd'] + ' - FINISHED')
+            logger.info(value['name'] + ' -ParamCode:' + query['parameterCd'] + ' - STARTED')
+            data_import=imp.load_source(item['source'],os.path.join(module_dir,'USGS'+'.py')) # __import__(os.path.join(pwd,'USGS'))#item['source'])
+            urls.append(data_import.save(value['name'],newDir,query))
+            logger.info(value['name'] + ' -ParamCode:' + query['parameterCd'] + ' - FINISHED')
         except Exception as inst:
-            logging.warning(inst)
+            logger.warning(inst)
             raise inst
-            #print "error !!!!!!!"
-            
-        
-#        if item['source']=='USGS':
-#            data_import= __import__(item['source'])
-#            temp = item
-#            temp.pop('source')
-#            if item['webservice_type']=='uv' or item['webservice_type']=='iv':
-#                temp.pop('webservice_type')
-#                temp['format']='json'
-#                print temp
-#                result=subtask('owsq.data.usgs.usgs_get_sitedata',(temp['sites'],), {'type':'instantaneous','params':temp,'save':'static'}) 
-#    return result
+    return filezip.makezip(urls, str(data_download.request.id), os.path.join(basedir,'request/'), overwrite=True)
