cd25f44d1e40d799e8731a72ec0d5522948c60c9
Mark Stacy <markstacy@ou.edu>
2013-04-30 10:49:37 -0500

    url parameters fixed

--- owsq/data/download/USGS.py
+++ owsq/data/download/USGS.py
@@ -101,7 +101,7 @@ def save_sitedata(name,path,query,data_provider='USGS-Tools-TypeSet',default_for
         if temp=='':
             temp = "%s%s%s" % (k,"=",v)
         else:
-            temp = "%s%s%s%s" % (temp,"&",k,"=",v)
+            temp = "%s%s%s%s%s" % (temp,"&",k,"=",v)
         #temp= temp + k + "=" + v + '&'
     if qtype=='qw':
         url =metadata['webservice'] + temp
