1c95abd27d568fe059f259226ad879ced24d3cd7
Mark Stacy <markstacy@ou.edu>
2013-05-18 14:12:58 -0500

    Revised task code Parallel subtasks

--- owsq/data/owrb.py
+++ owsq/data/owrb.py
@@ -8,11 +8,13 @@ from pymongo import Connection
 from cybercom.data.catalog import datacommons #catalog
 from owsq import config
 from owsq.util import gis_tools
-#from celery.task import subtask
-#from celery.task import group
+from celery.task import subtask
+from celery.task import group
 #set catalog user and passwd
 username = config.catalog_username #s.get('user','username')
 password = config.catalog_password #s.get('user','password')
+watershed=None
+aquifer =None
 
 @task()
 def owrb_sync_geojson(data_type='groundwater',database=config.owrb_database,tmp_fldr='/data/owrb/',data_provider='OWRB',delete=True):
@@ -65,88 +67,57 @@ def owrb_well_logs_save(database=config.owrb_database,collection=config.owrb_wel
     #load owrb well logs
     res=urllib2.urlopen(config.well_logs_url)
     data= json.loads(res.read())
-#    stask=[]
-#    taskname_tmpl='owsq.data.owrb.owrb_well_logs_sub'
-    i =0
-#    items=[]
+    stask=[]
+    taskname_tmpl='owsq.data.owrb.owrb_well_logs_portal'
     for site in data["features"]:
         row_data = {}
         row_data = site["properties"]
         row_data['geometry'] = site['geometry']
-        db[database][collection].save(row_data)
-        i=i+1
-        #stask.append(subtask(taskname_tmpl,args=(site,polydata,aquifer_poly,database,collection,),kwargs={}))
-        #items.append(site)
+        rowid=db[database][collection].save(row_data)
+        stask.append(subtask(taskname_tmpl,args=(rowid)))
+    print 'Done with inserts, starting group jobs'
+    job = group(stask)
+    result = job.apply_async() 
+    aggregate_results=result.join()
+    return "Success- All Well logs stored locally in Mongo(%s, %s) Total = %d" % (database,collection,sum(aggregate_results))  
 
-        #if i>=1000:
-        #    stask.append(subtask(taskname_tmpl,args=(list(items),polydata,aquifer_poly,database,collection,),kwargs={}))
-        #    items=[]
-        #    print i
-        #    i=1            
-        #i=i+1
-        #stask.append(subtask(taskname_tmpl,args=(site,polydata,aquifer_poly,database,collection,),kwargs={}))
-    #job = group(stask)
-    #result = job.apply_async() 
-    #aggregate_results=result.join()
-    return "Success- All Well logs stored locally in Mongo(%s, %s) Total = %d" % (database,collection,i)  
-        #row_data = {}
-        #row_data = site["properties"]
-        #r#ow_data['geometry'] = site['geometry']
-        #for poly in polydata:
-        #    s= poly['geometry']
-        #    if gis_tools.intersect_point(s,row_data['geometry']['coordinates'][1],row_data['geometry']['coordinates'][0],transform=False):
-        #        if 'HUC_4' in poly['properties']:
-        #            #print 'HUC 4: ' + poly['properties']['HUC_4']
-        #            row_data["huc_4"]=poly['properties']['HUC_4']
-        #        if 'HUC_8' in poly['properties']:
-                    #print 'HUC 8: ' + poly['properties']['HUC_8']
-        #            row_data["huc_8"]=poly['properties']['HUC_8']
-        #set aquifer data
-        #for poly in aquifer_poly:
-        #    s= poly['geometry']
-        #    if gis_tools.intersect_point(s,row_data['geometry']['coordinates'][1],row_data['geometry']['coordinates'][0],transform=False):
-        #        row_data["aquifer"]=poly['properties']['NAME']
-        #        print poly['properties']['NAME']
-        #        break
-        #db[database][collection].save(row_data)
 @task()
-def owrb_well_logs_portal(database=config.owrb_database, collection=config.owrb_welllog_collection, **kwargs):
+def owrb_well_logs_portal(rowid,database=config.owrb_database, collection=config.owrb_welllog_collection, **kwargs):
+    global watershed,aquifer
     db=Connection(config.mongo_host)
     #set watershed and aquifer geodata
-    polydata=[]
-    for itm in db.ows.watersheds.find():
-        polydata.append(itm)
-    aquifer_poly=[]
-    for itm in db.ows.aquifers.find():
-        aquifer_poly.append(itm) 
-    progress = 0
-    total=0
-    for row_data in db[database][collection].find():
-        for poly in polydata:
-            s= poly['geometry']
-            if gis_tools.intersect_point(s,row_data['LATITUDE'],row_data['LONGITUDE']):
-                if 'HUC_4' in poly['properties']:
-                    #print 'HUC 4: ' + poly['properties']['HUC_4']
-                    row_data["huc_4"]=poly['properties']['HUC_4']
-                if 'HUC_8' in poly['properties']:
-                    #print 'HUC 8: ' + poly['properties']['HUC_8']
-                    row_data["huc_8"]=poly['properties']['HUC_8']
-        #set aquifer data
-        for poly in aquifer_poly:
-            s= poly['geometry']
-            if gis_tools.intersect_point(s,row_data['LATITUDE'],row_data['LONGITUDE']):
-                row_data["aquifer"]=poly['properties']['NAME']
-                print poly['properties']['NAME']
-                break
-        db[database][collection].save(row_data)
-        progress=progress +1
-        if progress >=1000:
-            total = total + progress
-            progress = 0
-            print 'Records Complete %d' % (total)
-    return 'Records Complete %d' % (total + progress)
-#@task()
-#def owrb_well_logs_sync():
-#    result=owrb_well_logs_save.delay(callback=subtask(owrb_well_logs_portal))
-#    return result
+    if watershed:
+        polydata=watershed
+    else:
+        polydata=[]
+        for itm in db.ows.watersheds.find():
+            polydata.append(itm)
+        watershed = polydata
+    if aquifer:
+        aquifer_poly=aquifer
+    else:
+        aquifer_poly=[]
+        for itm in db.ows.aquifers.find():
+            aquifer_poly.append(itm)
+        aquifer = aquifer_poly 
+    row_data = db[database][collection].find_one({'_id':rowid})
+    for poly in polydata:
+        s= poly['geometry']
+        if gis_tools.intersect_point(s,row_data['LATITUDE'],row_data['LONGITUDE']):
+            if 'HUC_4' in poly['properties']:
+                row_data["huc_4"]=poly['properties']['HUC_4']
+            if 'HUC_8' in poly['properties']:
+                row_data["huc_8"]=poly['properties']['HUC_8']
+    #set aquifer data
+    for poly in aquifer_poly:
+        s= poly['geometry']
+        if gis_tools.intersect_point(s,row_data['LATITUDE'],row_data['LONGITUDE']):
+            row_data["aquifer"]=poly['properties']['NAME']
+            break
+    db[database][collection].save(row_data)
+    return 1 
+@task()
+def owrb_well_logs_sync():
+    result=owrb_well_logs_save.delay(callback=subtask(owrb_well_logs_portal))
+    return result
 
